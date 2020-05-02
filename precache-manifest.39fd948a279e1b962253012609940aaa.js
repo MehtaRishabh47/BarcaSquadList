b3c26359a79a574e10901de55b3cf08709debf45
Rishabh <rishabh.mehta98.rm@gmail.com>
2020-05-02 22:30:17 +0530

    Updates

---
+++ precache-manifest.39fd948a279e1b962253012609940aaa.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58d501261f459347b99a3b224b8dfe53",
+    "url": "/BarcaSquadList/index.html"
+  },
+  {
+    "revision": "c6ba734db04c0dd49d66",
+    "url": "/BarcaSquadList/static/css/2.ef6c8c45.chunk.css"
+  },
+  {
+    "revision": "f439dd61963ffd26ced3",
+    "url": "/BarcaSquadList/static/css/main.4d19cc8c.chunk.css"
+  },
+  {
+    "revision": "c6ba734db04c0dd49d66",
+    "url": "/BarcaSquadList/static/js/2.540abe38.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BarcaSquadList/static/js/2.540abe38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f439dd61963ffd26ced3",
+    "url": "/BarcaSquadList/static/js/main.9e6f4e1f.chunk.js"
+  },
+  {
+    "revision": "c6efc3c8617c758c2634",
+    "url": "/BarcaSquadList/static/js/runtime-main.2ef0680b.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/BarcaSquadList/static/media/SEGA.12a3db8b.woff"
+  }
+]);
